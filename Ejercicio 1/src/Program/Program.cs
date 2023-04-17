d584f8de3ef35c54bd95743a61ec42b5d792b5e1
Ramirods <ramiro.dasilva@correo.ucu.edu.uy>
2023-04-17 20:14:29 -0300

    Enviado

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,8 +9,19 @@ namespace SRP
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+
+            Sector sector1 = new Sector();
+            Sector sector2 = new Sector();
+
+            Shelve shelve1 = new Shelve();
+            Shelve shelve2 = new Shelve();
+
+            shelve1.BookAdd("7", book1);
+            shelve2.BookAdd("3", book2);
+
+            sector1.ShelveAdd("A", shelve1);
+            sector2.ShelveAdd("B", shelve2);
+
 
         }
     }
